ae11c6fdce3b30a4d0bb570edda130be33ae82a7
luebeck <kai.luebeck@smail.th-koeln.de>
2023-10-24 22:49:13 +0200

    change Conf

--- basicOutput.py
+++ basicOutput.py
@@ -7,34 +7,27 @@ import time
 
 UDP_IP_CLIENT = conf.UDP_IP_CLIENT
 UDP_PORT = conf.UDP_PORT
+SAMPLING_FREQUENCY = conf.SAMPLING_FREQUENCY
 
 frames = []
 
 def play_sound():
     global frames
-    filename = 'Teeinengland.wav'
 
-    # Set chunk size of 1024 samples per data frame
-    chunk = 1024  
-
-    # Open the sound file 
-    wf = wave.open(filename, 'rb')
-
-    # Create an interface to PortAudio
     p = pyaudio.PyAudio()
-
-    # Open a .Stream object to write the WAV file to
-    # 'output = True' indicates that the sound will be played rather than recorded
-    stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
-                    channels = wf.getnchannels(),
-                    rate = wf.getframerate(),
-                    output = True)
+    stream = p.open(format = pyaudio.paInt16,
+                    channels = 2,
+                    rate = SAMPLING_FREQUENCY,
+                    output = True, )
 
 
     # Play the sound by writing the audio data to the stream
-    time.sleep(3)
-    while len(frames) > 0:
-        stream.write(frames.pop(0))
+    while(len(frames) < 512):
+        time.sleep(0.1)
+    
+    while(len(frames) > 0):
+        stream.write(frames[:512])
+        frames = frames[512:]
 
     # Close and terminate the stream
     stream.close()
@@ -45,9 +38,8 @@ def get_sound():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
     sock.bind((UDP_IP_CLIENT, UDP_PORT))
     while True:
-        data, addr = sock.recvfrom(4 * 1024) # buffer size is 1024 bytes
-        frames.append(data)
-   #print("received message: %s" % data)
+        data, addr = sock.recvfrom(1472) # buffer size is 1024 bytes
+        frames.append(data[:])
 
 
 
--- basicStream.py
+++ basicStream.py
@@ -12,67 +12,27 @@ UDP_IP_CLIENT_RASPIZERO = '192.168.178.83'
 UDP_PORT = conf.UDP_PORT
 SAMPLING_FREQUENCY = conf.SAMPLING_FREQUENCY
 
-def udpStreamIn():
-    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    while True:
-        if len(frames) > 0:
-            data = frames.pop(0)
-            udp.sendto(data,  (UDP_IP_HOST, UDP_PORT))
-            time.sleep(CHUNK / SAMPLING_FREQUENCY)
-    udp.close()
+def audioInput():
+    wf = wave.open("Teeinengland.wav", 'rb')
+    data = wf.readframes(wf.getnframes())
+    return data
+    
 
-
-def record(stream, CHUNK):
-    while True:
-        frames.append(stream.read(CHUNK))
-
-def audio_stream():
+def audio_stream(data):
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp.bind((UDP_IP_HOST, UDP_PORT))
-    CHUNK = 1024
-    wf = wave.open("Teeinengland.wav", 'rb')
-    
-    p = pyaudio.PyAudio()
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    input=True,
-                    frames_per_buffer=CHUNK)
-    
-    print("Number of samples: ", wf.getnframes() * CHUNK)
-    nbyteswritten = 0
-    for i in range(500):
-        data = wf.readframes(CHUNK)
-        udp.sendto(data,  (UDP_IP_CLIENT_RASPIZERO, UDP_PORT))
-        time.sleep(CHUNK / SAMPLING_FREQUENCY)
-        nbyteswritten = nbyteswritten + len(data)
-    print("Number bytes send: ", nbyteswritten)
+    FRAMESIZE = 1472
+    while(len(data) > FRAMESIZE):
+        udp_packet = data[:FRAMESIZE]
+        data = data[FRAMESIZE:]
+        udp.sendto(udp_packet, (UDP_IP_CLIENT_RASPIZERO, UDP_PORT))
+        time.sleep((FRAMESIZE / 4) / SAMPLING_FREQUENCY)
 
     udp.close()
 
 if __name__ == '__main__':
-    CHUNK = 512
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 44100
 
-    # p = pyaudio.PyAudio()
-    # p = pyaudio.PyAudio()
-    # info = p.get_host_api_info_by_index(0)
-    # numdevices = info.get('deviceCount')
-
-    # for i in range(0, numdevices):
-    #     if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-    #         print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
-    # stream = p.open(format = FORMAT, channels = CHANNELS, rate = RATE,  input = True, frames_per_buffer = CHUNK , input_device_index=2)
-    # Tr = Thread(target = record, args = (stream, CHUNK,))
-    # Ts = Thread(target = udpStreamIn)
-    thread = Thread(target= audio_stream, args=())
-    thread.start()
-    thread.join()
-    # Tr.daemon = True
-    # Ts.daemon = True
-    # Tr.start()
-    # Ts.start()
-    # Tr.join()
-    # Ts.join()
+    data = audioInput()
+    #print(len(data))
+    print(type(data), data[0:10])
+    audio_stream(data)
