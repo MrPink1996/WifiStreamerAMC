554eb1ed5cd92b855680cb91f069a949d9dd9a82
luebeck <kai.luebeck@smail.th-koeln.de>
2023-10-26 23:36:14 +0200

    autoupload

--- basicOutput.py
+++ basicOutput.py
@@ -13,9 +13,10 @@ UDP_IP_CLIENT = conf.UDP_IP_CLIENT
 UDP_PORT = conf.UDP_PORT
 SAMPLING_FREQUENCY = conf.SAMPLING_FREQUENCY
 CHUNK_SIZE = conf.CHUNK_SIZE
-
+frames = []
 
 def play_sound():
+    global frames
     p = pyaudio.PyAudio()
     stream = p.open(format = pyaudio.paInt16,
                     channels = 2,
@@ -25,7 +26,8 @@ def play_sound():
 
     # Play the sound by writing the audio data to the stream
     while(True):
-        stream.write(q.get(), CHUNK_SIZE)
+        #stream.write(q.get(), CHUNK_SIZE)
+        stream.write(frames.pop(0), CHUNK_SIZE)
 
 
 def get_sound():
@@ -34,7 +36,8 @@ def get_sound():
 
     while (True):
         data, addr = sock.recvfrom(CHUNK_SIZE*2*2) # buffer size is 1024 bytes
-        q.put(data)
+        #q.put(data)
+        frames.append(data)
 
 if __name__ == "__main__":
     thread1 = Thread(target= get_sound, args=())
--- basicStream.py
+++ basicStream.py
@@ -16,27 +16,25 @@ SAMPLING_FREQUENCY = conf.SAMPLING_FREQUENCY
 CHUNK_SIZE = conf.CHUNK_SIZE
 BUFFER_SIZE = conf.BUFFER_SIZE
 
-
+frames = []
 def audioRecord():
+    global frames
     p = pyaudio.PyAudio()
-    # info = p.get_host_api_info_by_index(0)
-    # numdevices = info.get('deviceCount')
-    # for i in range(0, numdevices):
-    #     if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-    #         print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
-
     stream = p.open(format = pyaudio.paInt16,
                     channels = 2,
                     rate = SAMPLING_FREQUENCY,
                     input = True, input_device_index=2, frames_per_buffer=CHUNK_SIZE)
     
     while(True):
-        q.put(stream.read(CHUNK_SIZE, exception_on_overflow=False))
+        #q.put(stream.read(CHUNK_SIZE, exception_on_overflow=False))
+        frames.append(stream.read(CHUNK_SIZE, exception_on_overflow=False))
     
 def audioStream():
+    global frames
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while(True):
-        udp.sendto(q.get(), ('192.168.178.83', UDP_PORT))
+        udp.sendto(frames.pop(0), ('192.168.178.83', UDP_PORT))
+        #udp.sendto(q.get(), ('192.168.178.83', UDP_PORT))
     udp.close()
 
 def audioPlay():
@@ -58,7 +56,7 @@ def audioPlay():
 if __name__ == '__main__':
     recorder = Thread(target=audioRecord, args=())
     streamer = Thread(target=audioStream, args=())
-    player = Thread(target=audioPlay, args=())
+    #player = Thread(target=audioPlay, args=())
     recorder.daemon = True
     streamer.daemon = True
 
